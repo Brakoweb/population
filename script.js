5e04aba866a15b6c810e5a21cb785c7201b26bc9
Brako <gabobrako@gmail.com>
2023-10-07 14:27:02 -0400

    Define the data to be used for the chart

---
+++ script.js
@@ -0,0 +1,14 @@
+// Define the data to be used for the chart
+const data = [
+  { country: "United States", population: 332 },
+  { country: "China", population: 1444 },
+  { country: "India", population: 1392 },
+  { country: "Indonesia", population: 276 },
+  { country: "Pakistan", population: 225 },
+  { country: "Brazil", population: 213 },
+  { country: "Nigeria", population: 211 },
+  { country: "Bangladesh", population: 166 },
+  { country: "Russia", population: 146 },
+  { country: "Mexico", population: 130 },
+  { country: "Venezuela", population: 28 },
+];
